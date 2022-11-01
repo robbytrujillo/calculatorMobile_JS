0d899d668eda2e0ef3242ed45783ebc206206aa5
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-11-01 22:31:12 +0700

    Calculator Mobile Javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+//reqem = document.getElementById("mobileTotal").innerHTML;
+
+//Robby Ilhamkusuma
+//Mobile calculator
+
+function sec(nomre) {
+    reqem = document.getElementById("mobileTotal").innerHTML;
+    reqem = reqem + nomre;
+    document.getElementById("mobileTotal").innerHTML = reqem;
+}
+
